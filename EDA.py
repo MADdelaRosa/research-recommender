d33d51810e000205658bcc36b4f5043b15dfedc6
KrazyKat <diazdelarosa@gmail.com>
2017-07-05 15:11:38 -0700

    Dowload data EDA

--- EDA.py
+++ EDA.py
@@ -25,3 +25,28 @@ dld_comp = dld[dld.user_account_status == 'complimentary_account']
 # filter out paid members that are within the company:
 mask = dld_paid[dld_paid['email'].str.contains('deloitte')]
 dld_ext = dld_paid.drop(mask.index)
+
+# Explore download history:
+# Download histogram
+dld_ext.research_id.hist(grid=False)
+plt.xlabel('Research ID')
+plt.ylabel('Count')
+#plt.show()
+# Most downloaded:
+histos = dld_ext.research_id.value_counts()
+id_most = histos.index[0]
+ind_most = rmd[rmd.content_id == id_most].index[0]
+print 'Most downloaded research_id: ', id_most
+print 'Title: ', rmd.content_title[ind_most]
+print 'Long description: ', rmd.long_description[ind_most]
+
+def most_downloaded(download_df, research_df, number):
+    counts = download_df.research_id.value_counts()
+    for i in xrange(number):
+        res_id = int(counts.index[i])
+        ind = research_df[research_df.content_id == res_id].index[0]
+        print 'The number {} most downloaded research_id: {}'.format(i+1, res_id)
+        print 'Title: ', research_df.content_title[ind]
+        print 'Author: ', research_df.author_ordered[ind]
+        print 'Long description: ', research_df.long_description[ind]
+        print '\n'
