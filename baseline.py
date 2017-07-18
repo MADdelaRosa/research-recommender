d5acf66d2825026db16f8cbe0ba1bbf1ca893603
KrazyKat <diazdelarosa@gmail.com>
2017-07-18 14:59:10 -0700

    Presentation, Tuesday

--- baseline.py
+++ baseline.py
@@ -420,3 +420,17 @@ if __name__ == '__main__':
 
     end_time = time()
     print "Time elapsed: ", end_time - start_time
+
+    # '''How to find index of elements of an array inside another array:'''
+    # x = np.array([3,5,7,1,9,8,6,6])
+    # y = np.array([2,1,5,10,100,6])
+    # 
+    # index = np.argsort(x)
+    # sorted_x = x[index]
+    # sorted_index = np.searchsorted(sorted_x, y)
+    #
+    # yindex = np.take(index, sorted_index, mode="clip")
+    # mask = x[yindex] != y
+    #
+    # result = np.ma.array(yindex, mask=mask)
+    # print result
